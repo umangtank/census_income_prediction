e35161ef5832deded24ecb289e04b032b259d8bc
umangtank <umangtank08@gmail.com>
2021-08-15 23:07:45 +0530

    Added

---
+++ README.md
@@ -0,0 +1,32 @@
+
+# 👩🏻‍💻Census Income Prediction
+
+**[pred-census.herokuapp.com](https://pred-census.herokuapp.com/)**
+
+- This repository represents " Census income prediction ".
+- With the help of this project we can predict whether a person has an income of more than 50K a year or not.
+## Dataset
+
+- **Here's a brief version of what you'll find in the data file.** [Dataset](https://www.kaggle.com/overload10/adult-census-dataset)
+
+| Features | Value     | 
+| :-------- | :------- | 
+| `age` | continuous. | 
+| `workclass` | Private, Self-emp-not-inc, Self-emp-inc, Federal-gov, Local-gov, State-gov, Without-pay, Never-worked.|
+| `education` | Bachelors, Some-college, 11th, HS-grad, Prof-school, Assoc-acdm, Assoc-voc, 9th, 7th-8th, 12th, Masters, 1st-4th, 10th, Doctorate, 5th-6th, Preschool |
+| `education-num`| continuous |
+| `marital-status`| Married-civ-spouse, Divorced, Never-married, Separated, Widowed, Married-spouse-absent, Married-AF-spouse. |
+| `occupation`| Tech-support, Craft-repair, Other-service, Sales, Exec-managerial, Prof-specialty, Handlers-cleaners, Machine-op-inspct, Adm-clerical, Farming-fishing, Transport-moving, Priv-house-serv, Protective-serv, Armed-Forces. |
+| `relationship` | Wife, Own-child, Husband, Not-in-family, Other-relative, Unmarried. |
+| `race`| White, Asian-Pac-Islander, Amer-Indian-Eskimo, Other, Black. |
+| `sex`| Female, Male. |
+| `capital-gain` | continuous |
+| `capital-loss`| continuous |
+| `hours-per-week`| continuous |
+| `Native-country`| All countries |
+
+
+
+
+
+  
---
+++ app.py
@@ -0,0 +1,90 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+
+pkl_file = open('models\model.pkl','rb')
+model = pickle.load(open('models\model.pkl', 'rb'))
+index_dict = pickle.load(pkl_file)
+
+
+app = Flask(__name__)
+@app.route('/')
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/submit',methods=['POST','GET'])
+def predict():
+
+    index_dict = pickle.load(open('models\cat','rb'))
+    country_cat = pickle.load(open('models\country_cat','rb'))
+    merital = pickle.load(open('models\merital','rb'))
+    education = pickle.load(open('models\education','rb'))
+    sex = pickle.load(open('models\sex','rb'))
+    relation = pickle.load(open('models\\relation','rb'))
+    occupation = pickle.load(open('models\occupation','rb'))
+    work = pickle.load(open('models\work','rb'))
+    race = pickle.load(open('models\\race','rb'))
+
+
+    if request.method == "POST":
+        age = request.form['age']
+        capital_gain = request.form['capital_gain']
+        capital_loss = request.form['capital_loss']
+        education_num = request.form['education_num']
+        hours = request.form['hours']
+        sexs = request.form['sexs']
+        occupations = request.form['occupations']
+        works = request.form['works']
+        educations = request.form['educations']
+        races = request.form['races']
+        country = request.form['country']
+        meritals = request.form['meritals']
+        relations = request.form['relations']
+
+
+        output = np.zeros(108)
+        output[0] = age
+        output[2] = capital_gain
+        output[3] = capital_loss
+        output[1] = education_num
+        output[4] = hours
+        
+        re_sex = sexs
+        if re_sex not in sex:
+            pass
+        else:
+            output[index_dict[str(sexs)]] = 1
+
+        result_location = meritals
+        output[index_dict[str(meritals)]] = 1
+            
+        result_location = country
+        if result_location not in country_cat:
+            output[34] = 1
+        else:
+            output[index_dict[str(country)]] = 1
+
+        result_location = races
+        if result_location not in race:
+            output[77] = 1
+        else:
+            output[index_dict[str(races)]] = 1
+            
+        result_location = works
+        output[index_dict[str(works)]] = 1
+            
+        result_location = occupations
+        if result_location not in occupation:
+            output[93] = 1
+            output[index_dict[str(occupations)]] = 1
+
+        result_location = educations
+        output[index_dict[str(educations)]] = 1
+            
+        result_location = relations
+        output[index_dict[str(relations)]] = 1  
+        models = model.predict([output])[0]
+        return render_template("result.html",models = models)
+
+if __name__ == '__main__':
+    app.run(debug=True)
