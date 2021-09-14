97900d919705923b1e3790bdcb21f805e100c6f8
umangtank <umangtank08@gmail.com>
2021-09-15 00:48:04 +0530

    Modular approach

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from flask import Flask,render_template,request
 import pickle
 import numpy as np
 from flask_mysqldb import MySQL
+from utils.all_utils import Pred,Mysql
 
-pkl_file = open('models\model.pkl','rb')
 model = pickle.load(open('models\model.pkl', 'rb'))
-index_dict = pickle.load(pkl_file)
-
 
 app = Flask(__name__)
 
@@ -23,18 +21,7 @@ def welcome():
 
 @app.route('/submit',methods=['POST','GET'])
 def predict():
-
-    index_dict = pickle.load(open('models\cat','rb'))
-    country_cat = pickle.load(open('models\country_cat','rb'))
-    merital = pickle.load(open('models\merital','rb'))
-    education = pickle.load(open('models\education','rb'))
-    sex = pickle.load(open('models\sex','rb'))
-    relation = pickle.load(open('models\\relation','rb'))
-    occupation = pickle.load(open('models\occupation','rb'))
-    work = pickle.load(open('models\work','rb'))
-    race = pickle.load(open('models\\race','rb'))
-
-
+    ## Collecting infrmation from user
     if request.method == "POST":
         age = request.form['age']
         capital_gain = request.form['capital_gain']
@@ -50,53 +37,14 @@ def predict():
         meritals = request.form['meritals']
         relations = request.form['relations']
 
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO userinput(age, capital_gain, capital_loss, education_year,working_hours, occupation, sex, merital, country, race, work_class, education, relation) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (age, capital_gain, capital_loss, education_num, hours, occupations, sexs, meritals, country, races, works, educations, relations))
-        mysql.connection.commit()
-        cur.close()
 
-        output = np.zeros(108)
-        output[0] = age
-        output[2] = capital_gain
-        output[3] = capital_loss
-        output[1] = education_num
-        output[4] = hours
-        
-        re_sex = sexs
-        if re_sex not in sex:
-            pass
-        else:
-            output[index_dict[str(sexs)]] = 1
+        ## Add All the input data in database
+        Mysql(mysql,age, capital_gain, capital_loss, education_num, hours, occupations, sexs, meritals, country, races, works, educations, relations)
 
-        result_location = meritals
-        output[index_dict[str(meritals)]] = 1
-            
-        result_location = country
-        if result_location not in country_cat:
-            output[34] = 1
-        else:
-            output[index_dict[str(country)]] = 1
-
-        result_location = races
-        if result_location not in race:
-            output[77] = 1
-        else:
-            output[index_dict[str(races)]] = 1
-            
-        result_location = works
-        output[index_dict[str(works)]] = 1
-            
-        result_location = occupations
-        if result_location not in occupation:
-            output[93] = 1
-            output[index_dict[str(occupations)]] = 1
-
-        result_location = educations
-        output[index_dict[str(educations)]] = 1
-            
-        result_location = relations
-        output[index_dict[str(relations)]] = 1  
+        ## Predicting Income based on user input
+        output = Pred(age, capital_gain, capital_loss, education_num, hours, occupations, sexs, meritals, country, races, works, educations, relations)
         models = model.predict([output])[0]
+
         return render_template("result.html",models = models)
 
 if __name__ == '__main__':
---
+++ utils/all_utils.py
@@ -0,0 +1,65 @@
+import pickle
+import numpy as np
+
+def Pred(age, capital_gain, capital_loss, education_num, hours, occupations, sexs, meritals, country, races, works, educations, relations):
+    
+    index_dict = pickle.load(open('models\cat','rb'))
+    country_cat = pickle.load(open('models\country_cat','rb'))
+    merital = pickle.load(open('models\merital','rb'))
+    education = pickle.load(open('models\education','rb'))
+    sex = pickle.load(open('models\sex','rb'))
+    relation = pickle.load(open('models\\relation','rb'))
+    occupation = pickle.load(open('models\occupation','rb'))
+    work = pickle.load(open('models\work','rb'))
+    race = pickle.load(open('models\\race','rb'))
+
+    output = np.zeros(108)
+    output[0] = age
+    output[2] = capital_gain
+    output[3] = capital_loss
+    output[1] = education_num
+    output[4] = hours
+    
+    re_sex = sexs
+    if re_sex not in sex:
+        pass
+    else:
+        output[index_dict[str(sexs)]] = 1
+
+    result_location = meritals
+    output[index_dict[str(meritals)]] = 1
+    
+    result_location = country
+    if result_location not in country_cat:
+        output[34] = 1
+    else:
+        output[index_dict[str(country)]] = 1
+
+    result_location = races
+    if result_location not in race:
+        output[77] = 1
+    else:
+        output[index_dict[str(races)]] = 1
+            
+    result_location = works
+    output[index_dict[str(works)]] = 1
+            
+    result_location = occupations
+    if result_location not in occupation:
+        output[93] = 1
+    else:
+        output[index_dict[str(occupations)]] = 1
+
+    result_location = educations
+    output[index_dict[str(educations)]] = 1
+
+    result_location = relations
+    output[index_dict[str(relations)]] = 1
+    
+    return output
+
+def Mysql(mysql,age, capital_gain, capital_loss, education_num, hours, occupations, sexs, meritals, country, races, works, educations, relations):
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO userinput(age, capital_gain, capital_loss, education_year,working_hours, occupation, sex, merital, country, race, work_class, education, relation) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (age, capital_gain, capital_loss, education_num, hours, occupations, sexs, meritals, country, races, works, educations, relations))
+    mysql.connection.commit()
+    cur.close()
